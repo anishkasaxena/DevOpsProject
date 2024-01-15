e7a7e0e2e102232fb9596bc19ee247f7906b73bf
anishkasaxena <anishka@5ire.org>
2024-01-15 23:04:18 +0530

    fix pipeline

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,7 +1,24 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+
+# backend "remote" {
+# 		hostname = "app.terraform.io"
+# 		organization = "Devsecops-org"
+
+# 		workspaces {
+# 			name = "Devsecops-org"
+# 		}
+# 	}
+# }
 terraform {
   required_providers {
     aws = {
@@ -10,12 +27,12 @@ terraform {
     }
   }
 
-backend "remote" {
-		hostname = "app.terraform.io"
-		organization = "Devsecops-org"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Devsecops-org"
 
-		workspaces {
-			name = "Devsecops-org"
-		}
-	}
+    workspaces {
+      name = "Devsecops-org"
+    }
+  }
 }
