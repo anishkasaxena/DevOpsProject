37796bc555a9ea726fd8fb3b5c05a500b529b1e6
Anishka <112534892+anishkasaxena@users.noreply.github.com>
2024-01-16 02:47:39 +0530

    Featurebranch (#4)
    * Task 3 build pipeline
    
    * Task 3,4,5 build pipeline added autoscale
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes
    
    * error fixes

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,12 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Devsecops-org"
+
+    workspaces {
+      name = "Devsecops-org"
     }
   }
 }
